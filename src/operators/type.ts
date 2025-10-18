4dca8c670335aacd9cd283ecb592dd2c9ce05b70
Yunus Alper Göl <yunus.alper.gl@gmail.com>
2025-10-19 00:37:04 +0300

    feat: register type oeprators

---
+++ src/operators/type.ts
@@ -0,0 +1,30 @@
+import isString from 'lodash/isString';
+import isNumber from 'lodash/isNumber';
+import isBoolean from 'lodash/isBoolean';
+import isArray from 'lodash/isArray';
+import isPlainObject from 'lodash/isPlainObject';
+import { registerOperator } from '.';
+
+export function registerTypeOperators(): void {
+  registerOperator('isString', (fieldValue: unknown): boolean => {
+    return isString(fieldValue);
+  });
+
+  registerOperator('isNumber', (fieldValue: unknown): boolean => {
+    return isNumber(fieldValue) && !Number.isNaN(fieldValue);
+  });
+
+  registerOperator('isBoolean', (fieldValue: unknown): boolean => {
+    return isBoolean(fieldValue);
+  });
+
+  registerOperator('isArray', (fieldValue: unknown): boolean => {
+    return isArray(fieldValue);
+  });
+
+  registerOperator('isObject', (fieldValue: unknown): boolean => {
+    return isPlainObject(fieldValue);
+  });
+}
+
+registerTypeOperators();

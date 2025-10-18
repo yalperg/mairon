119161435ee5dda83855a6e71bac55c7111cf5f3
Yunus Alper Göl <yunus.alper.gl@gmail.com>
2025-10-19 00:33:33 +0300

    feat: register array operators

---
+++ src/operators/array.ts
@@ -0,0 +1,54 @@
+import isArray from 'lodash/isArray';
+import isEmpty from 'lodash/isEmpty';
+import intersection from 'lodash/intersection';
+import { registerOperator } from '.';
+import { SimpleCondition } from '../core/types';
+
+export function registerArrayOperators(): void {
+  registerOperator('includes', (fieldValue: unknown, condition: SimpleCondition): boolean => {
+    if (!isArray(fieldValue)) {
+      return false;
+    }
+    return fieldValue.includes(condition.value);
+  });
+
+  registerOperator('excludes', (fieldValue: unknown, condition: SimpleCondition): boolean => {
+    if (!isArray(fieldValue)) {
+      return false;
+    }
+    return !fieldValue.includes(condition.value);
+  });
+
+  registerOperator('includesAll', (fieldValue: unknown, condition: SimpleCondition): boolean => {
+    if (!isArray(fieldValue) || !isArray(condition.value)) {
+      return false;
+    }
+    const required = condition.value;
+    const common = intersection(fieldValue, required);
+    return common.length === required.length;
+  });
+
+  registerOperator('includesAny', (fieldValue: unknown, condition: SimpleCondition): boolean => {
+    if (!isArray(fieldValue) || !isArray(condition.value)) {
+      return false;
+    }
+    const common = intersection(fieldValue, condition.value);
+    return common.length > 0;
+  });
+
+  registerOperator('isEmpty', (fieldValue: unknown): boolean => {
+    if (!isArray(fieldValue)) {
+      return false;
+    }
+    return isEmpty(fieldValue);
+  });
+
+  registerOperator('isNotEmpty', (fieldValue: unknown): boolean => {
+    if (!isArray(fieldValue)) {
+      return false;
+    }
+    return !isEmpty(fieldValue);
+  });
+}
+
+registerArrayOperators();

ca539778e00d57584d22f7db8153b55b9d6b659f
Yunus Alper Göl <yunus.alper.gl@gmail.com>
2025-10-19 00:33:45 +0300

    feat: register existence operators

---
+++ src/operators/existence.ts
@@ -0,0 +1,31 @@
+import isNull from 'lodash/isNull';
+import isUndefined from 'lodash/isUndefined';
+import { registerOperator } from './index';
+
+export function registerExistenceOperators(): void {
+  registerOperator('exists', (fieldValue: unknown): boolean => {
+    return fieldValue !== undefined;
+  });
+
+  registerOperator('notExists', (fieldValue: unknown): boolean => {
+    return fieldValue === undefined;
+  });
+
+  registerOperator('isNull', (fieldValue: unknown): boolean => {
+    return isNull(fieldValue);
+  });
+
+  registerOperator('isNotNull', (fieldValue: unknown): boolean => {
+    return !isNull(fieldValue);
+  });
+
+  registerOperator('isDefined', (fieldValue: unknown): boolean => {
+    return !isUndefined(fieldValue);
+  });
+
+  registerOperator('isUndefined', (fieldValue: unknown): boolean => {
+    return isUndefined(fieldValue);
+  });
+}
+
+registerExistenceOperators();
